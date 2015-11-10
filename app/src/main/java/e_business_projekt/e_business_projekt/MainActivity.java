5f0e466ab40cba8cffcc65e655ba0dc9d8f6fe73
Sebastian <s59474@beuth-hochschule.de>
2015-11-10 13:08:20 +0100

    added Vuforia.jar

---
+++ app/src/main/java/e_business_projekt/e_business_projekt/MainActivity.java
@@ -0,0 +1,13 @@
+package e_business_projekt.e_business_projekt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
