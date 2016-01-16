7055d3c32892e0cb3dcf9e228d965308871bc5c4
Sebastian <s59474@beuth-hochschule.de>
2016-01-16 15:43:09 +0100

    implemented navigation between two destinations added several classes from this example http://stackoverflow.com/questions/11745314/why-retrieving-google-directions-for-android-using-kml-data-is-not-working-anymo/11745316#11745316 and  https://github.com/jd-alexander/Google-Directions-Android

---
+++ app/src/main/java/e_business_projekt/e_business_projekt/Maps_Navigation/Parser.java
@@ -0,0 +1,8 @@
+package e_business_projekt.e_business_projekt.Maps_Navigation;
+
+import java.util.List;
+
+//. by Haseem Saheed
+public interface Parser {
+    List<Route> parse();
+}
---
+++ app/src/main/java/e_business_projekt/e_business_projekt/Maps_Navigation/RoutingListener.java
@@ -0,0 +1,13 @@
+package e_business_projekt.e_business_projekt.Maps_Navigation;
+
+import java.util.ArrayList;
+
+public interface RoutingListener {
+    void onRoutingFailure();
+
+    void onRoutingStart();
+
+    void onRoutingSuccess(ArrayList<Route> route, int shortestRouteIndex);
+
+    void onRoutingCancelled();
+}
