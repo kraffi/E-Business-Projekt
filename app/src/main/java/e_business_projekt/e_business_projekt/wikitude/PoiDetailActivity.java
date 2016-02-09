12f995dcbc34972844eebb1f1148e9737dc11a67
Sebastian <s59474@beuth-hochschule.de>
2016-02-09 15:10:08 +0100

    working on integration of routes to wikitude adding radar and details activity details activity works webview works

--- app/src/main/java/e_business_projekt/e_business_projekt/wikitude/PoiDetailActivity.java
+++ app/src/main/java/e_business_projekt/e_business_projekt/wikitude/PoiDetailActivity.java
@@ -1,14 +1,18 @@
 package e_business_projekt.e_business_projekt.wikitude;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
 
 import e_business_projekt.e_business_projekt.R;
+import e_business_projekt.e_business_projekt.WebViewActivity;
 import e_business_projekt.e_business_projekt.poi_list.PointOfInterest;
 import e_business_projekt.e_business_projekt.route_list.POIRouteProvider;
 
@@ -19,6 +23,7 @@ public class PoiDetailActivity extends AppCompatActivity {
 	public static final String EXTRAS_KEY_POI_TITILE = "name";
 	public static final String EXTRAS_KEY_POI_DESCR = "description";
     public String poi_id;
+    public String website;
 	//KR:
 	protected List<PointOfInterest> POIList;
     private PointOfInterest active_poi;
@@ -48,9 +53,30 @@ public class PoiDetailActivity extends AppCompatActivity {
 
 		((TextView)findViewById(R.id.addressTextView_pd)).setText(active_poi.getAddress());
         ((TextView)findViewById(R.id.phoneTextView_pd)).setText(active_poi.getPhonenumber());
-        ((TextView)findViewById(R.id.websiteTextView_pd)).setText(active_poi.getWebsiteUri().toString());
 
+        website = active_poi.getWebsiteUri().toString();
 
+        TextView websiteText = (TextView)findViewById(R.id.websiteTextView_pd);
+        if ((website.length() == 0) || website.equals("www.google.com")){
+            websiteText.setVisibility(View.GONE);
+            findViewById(R.id.websiteImageView_pd).setVisibility(View.GONE);
+        } else {
+            //websiteText.setText(website);
+            websiteText.setText(Html.fromHtml("<a href=\"" + website + "\">" + website + "</a>"));
+            //websiteText.setMovementMethod(LinkMovementMethod.getInstance());
+
+            //((TextView)findViewById(R.id.websiteTextView_pd)).setText(active_poi.getWebsiteUri().toString());
+        }
+
+        websiteText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent( PoiDetailActivity.this, WebViewActivity.class);
+                intent.putExtra("url", website);
+                startActivity(intent);
+            }
+        });
     }
 
 }
